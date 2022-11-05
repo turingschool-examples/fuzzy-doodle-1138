02914c3db8596f7af05ea555c6676a1db4cd7913
Amanda Ross <amikaross@gmail.com>
2022-11-05 12:24:36 -0600

    Test/feat: Add rides_by_experience instance method to AmusementPark model

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -5,4 +5,8 @@ class AmusementPark < ApplicationRecord
   def list_of_mechanics_names
     mechanics.distinct.pluck(:name)
   end
+
+  def rides_by_experience
+    rides.joins(:mechanics).group(:id).select("rides.*, avg(mechanics.years_experience) as avg_experience").order("avg_experience desc")
+  end
 end
--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -38,5 +38,38 @@ RSpec.describe AmusementPark, type: :model do
         expect(park.list_of_mechanics_names).to eq(["Haku Dog", "Hina Pup", "Kiko Dog", "Mako Cat", "Toro Kitty"])
       end
     end
+
+    describe "#rides_by_experience" do 
+      it "should return a list of the park's rides, ordered by the average experience of the mechanics, and including a column 'avg_experience" do 
+        park = AmusementPark.create!(name: "Six Flags", admission_cost: 25)
+        sizzler = park.rides.create!(name: "Sizzler", thrill_rating: 2, open: true)
+        swings = park.rides.create!(name: "Swings", thrill_rating: 1, open: false)
+        boat = park.rides.create!(name: "Pharoahs Boat", thrill_rating: 4, open: true)
+        haku = Mechanic.create!(name: "Haku Dog", years_experience: 1)
+        mako = Mechanic.create!(name: "Mako Cat", years_experience: 2)
+        kiko = Mechanic.create!(name: "Kiko Dog", years_experience: 3)
+        hina = Mechanic.create!(name: "Hina Pup", years_experience: 4)
+        toro = Mechanic.create!(name: "Toro Kitty", years_experience: 5)
+
+        RideMechanic.create!(mechanic: haku, ride: sizzler)
+        RideMechanic.create!(mechanic: mako, ride: sizzler)
+        RideMechanic.create!(mechanic: kiko, ride: sizzler)
+
+        RideMechanic.create!(mechanic: mako, ride: swings)
+        RideMechanic.create!(mechanic: kiko, ride: swings)
+        RideMechanic.create!(mechanic: hina, ride: swings)
+
+        RideMechanic.create!(mechanic: hina, ride: boat)
+        RideMechanic.create!(mechanic: kiko, ride: boat)
+        RideMechanic.create!(mechanic: toro, ride: boat)
+
+        result = park.rides_by_experience
+
+        expect(result).to eq([boat, swings, sizzler])
+        expect(result[0].avg_experience).to eq(4)
+        expect(result[1].avg_experience).to eq(3)
+        expect(result[2].avg_experience).to eq(2)
+      end
+    end
   end
 end
