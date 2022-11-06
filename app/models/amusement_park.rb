47f9f4b5b1ff843e5756c73e5090a3d7913931cd
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-06 11:34:37 -0700

    Adds park model

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,3 +1,8 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
+
+  def self.mechanics_on_the_job(park)
+    find_by_sql(["select distinct mechanics.name as mechanic_name, amusement_parks.name as park_name, rides.name as ride_name from amusement_parks join rides on rides.amusement_park_id = amusement_parks.id join ride_mechanics on rides.id = ride_mechanics.ride_id join mechanics on mechanics.id = ride_mechanics.mechanic_id where amusement_parks.id = ?", park.id])
+  end
+
 end
