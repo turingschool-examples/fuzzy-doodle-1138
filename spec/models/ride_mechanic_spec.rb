529934bd9bce0b89d1e0c6c462215b64c4a3f1d5
Mostafa Sakr <msakr3@gmail.com>
2022-11-08 15:54:23 -0700

    Create models and add relations.

--- spec/models/ride_mechanic_spec.rb
+++ spec/models/ride_mechanic_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Ride, type: :model do
+RSpec.describe RideMechanic, type: :model do
   describe 'relationships' do
     it { should belong_to(:mechanic) }
     it { should belong_to(:ride) }
