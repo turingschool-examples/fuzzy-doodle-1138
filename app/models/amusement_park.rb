7cb1ccda7f3a5232ffc377d6e88d172539ca1dc7
Mostafa Sakr <msakr3@gmail.com>
2022-11-08 16:46:44 -0700

    Add model test and method for US3.

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,3 +1,8 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
+
+  def mechanics_list
+    # binding.pry 
+    Mechanic.joins(:rides).where("rides.amusement_park_id = #{self.id}").distinct
+  end
 end
--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -4,4 +4,31 @@ RSpec.describe AmusementPark, type: :model do
   describe 'relationships' do
     it { should have_many(:rides) }
   end
+
+  describe 'methods' do
+    before :each do
+      @sam = Mechanic.create!(name: "Sam", years_experience: 11)
+      @abbas = Mechanic.create!(name: "Abbas", years_experience: 9)
+      @third_wheel = Mechanic.create!(name: "Third Guy", years_experience: 9)
+      @amusement_park = AmusementPark.create!(name: "Dreamland", admission_cost: 50)
+      @amusement_park_2 = AmusementPark.create!(name: "Wonderland", admission_cost: 50)
+      @ride_1 = @amusement_park.rides.create!(name: "Barf-a-lot", thrill_rating: 10, open: true)
+      @ride_2 = @amusement_park.rides.create!(name: "Merry Go Round", thrill_rating: 10, open: false)
+      @ride_3 = @amusement_park.rides.create!(name: "Gummi Coaster", thrill_rating: 8, open: true)
+      @ride_4 = @amusement_park_2.rides.create!(name: "House of Heartless", thrill_rating: 6, open: false)
+      @ride_5 = @amusement_park_2.rides.create!(name: "Blackpearl", thrill_rating: 7, open: true)
+      @sam.rides << @ride_1
+      @sam.rides << @ride_2
+      @abbas.rides << @ride_3
+      @sam.rides << @ride_4
+      @third_wheel.rides << @ride_5
+    end
+
+    describe '#mechanics_list' do
+      it 'returns a list of the mechanics(distinct) that work for an amusement park' do
+        expect(@amusement_park.mechanics_list).to include(@sam, @abbas)
+        expect(@amusement_park.mechanics_list.length).to eq(2)
+      end
+    end
+  end
 end
