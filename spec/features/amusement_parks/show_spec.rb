7c07807570560cc2dfe46e1e5ee0de3eb365da1e
James White <whitejaad@gmail.com>
2022-11-07 21:54:17 -0800

    3/4 through User story 3

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'amusement show page' do
       amusement_park_2 = AmusementPark.create!(name: "Oak Park", admission_cost: 50)
       amusement_park_3 = AmusementPark.create!(name: "The updside down", admission_cost: 60)
       amusement_park_4 = AmusementPark.create!(name: "Disney", admission_cost: 750)
-
+      
       mechanic_1 = Mechanic.create!(name: "Jim", years_of_experience: 22)
       mechanic_2 = Mechanic.create!(name: "Norm", years_of_experience: 30)
 
@@ -26,4 +26,19 @@ RSpec.describe 'amusement show page' do
   end
   
    it 'Lists all the mechanics working on that parks rides without repeating' do 
+    amusement_park_1 = AmusementPark.create!(name: "Six Flags", admission_cost: 75)
+    # amusement_park_2 = AmusementPark.create!(name: "Oak Park", admission_cost: 50)
+    # amusement_park_3 = AmusementPark.create!(name: "The updside down", admission_cost: 60)
+    # amusement_park_4 = AmusementPark.create!(name: "Disney", admission_cost: 750)
+      
+    mechanic_1 = Mechanic.create!(name: "Jim", years_of_experience: 22)
+    mechanic_2 = Mechanic.create!(name: "Norm", years_of_experience: 30)
+    
+    ride_1 = mechanic_1.rides.create!(name: "Top Gun", thrill_rating: 10, open: true, amusement_park_id: amusement_park_1.id)
+    ride_2 = mechanic_1.rides.create!(name: "Gravatron", thrill_rating: 8, open: true, amusement_park_id: amusement_park_1.id)
+    ride_3 = mechanic_2.rides.create!(name: "Fast and the Furious", thrill_rating: 6, open: true, amusement_park_id: amusement_park_1.id)
+    ride_4 = mechanic_2.rides.create!(name: "Joust", thrill_rating: 7, open: true, amusement_park_id: amusement_park_1.id)
+    
+    #expect(page).to have_content(mechanic_1.name, )
+  end
 end      
