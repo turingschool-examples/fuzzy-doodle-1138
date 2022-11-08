dd91d4cd9f537f823d7a68499df56ec3a6a90058
Alex M <alexm0717@hotmail.com>
2022-11-07 19:50:49 -0800

    relationships complete

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,7 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Ride, type: :model do
-  describe 'relationships' do
-    it { should belong_to(:amusement_park) }
+
+RSpec.describe(Ride, type: :model) do
+  describe("relationships") do
+    it { should(belong_to(:amusement_park)) }
+    it { should(have_many(:mechanic_rides)) }
+    it { should(have_many(:mechanics).through(:mechanic_rides)) }
   end
 end
