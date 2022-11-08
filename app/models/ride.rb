b45bb60bd5c66ac8c1aa33de094bafe1b879c5b6
Darby Smith <newman.darby2@gmail.com>
2022-11-08 09:08:14 -0500

    Add relationship to mechanic_rides

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,5 +1,7 @@
 class Ride < ApplicationRecord
   belongs_to :amusement_park
+  has_many :mechanic_rides
+  has_many :mechanics, through: :mechanic_rides
 
   validates_presence_of :name
   validates_numericality_of :thrill_rating
