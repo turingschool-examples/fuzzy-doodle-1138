f7cf64a7e7728ce5372d590753d6de9096012106
Darby Smith <newman.darby2@gmail.com>
2022-11-07 20:03:03 -0500

    Feat: add ride to mechanic

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,4 +2,12 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
   end
+
+  def update
+    mechanic = Mechanic.find(params[:id])
+    ride = Ride.find(params[:ride_id])
+    MechanicRide.create!(mechanic: mechanic, ride: ride)
+
+    redirect_to "/mechanics/#{mechanic.id}"
+  end
 end
