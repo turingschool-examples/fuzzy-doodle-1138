66993825ac35df7de76d1d0d8c52a832c5062782
Madeline Mauser <madeline.mauser@gmail.com>
2022-11-08 17:11:34 -0700

    Add test for amusement park method .park_mechanics

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -4,4 +4,31 @@ RSpec.describe AmusementPark, type: :model do
   describe 'relationships' do
     it { should have_many(:rides) }
   end
+
+  describe 'instance methods' do
+    before :each do
+      @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+      @universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
+
+      @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+      @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+      @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+      @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+
+      @kara = Mechanic.create!(name: 'Kara Smith', years_experience: 11)
+      @ted = Mechanic.create!(name: 'Ted Redd', years_experience: 6)
+      @maria = Mechanic.create!(name: 'Maria Black', years_experience: 8)
+
+      @mechanic_ride_1 = MechanicRide.create!(mechanic_id: @kara.id, ride_id: @hurler.id)
+      @mechanic_ride_2 = MechanicRide.create!(mechanic_id: @kara.id, ride_id: @scrambler.id)
+      @mechanic_ride_3 = MechanicRide.create!(mechanic_id: @kara.id, ride_id: @jaws.id)
+      @mechanic_ride_4 = MechanicRide.create!(mechanic_id: @ted.id, ride_id: @ferris.id)
+      @mechanic_ride_5 = MechanicRide.create!(mechanic_id: @maria.id, ride_id: @jaws.id)
+    end
+    describe '.park_mechanics' do
+      it 'should return all unique mechanics working on rides at the park' do
+        expect(@six_flags.park_mechanics).to eq([@kara, @ted])
+      end
+    end
+  end
 end
